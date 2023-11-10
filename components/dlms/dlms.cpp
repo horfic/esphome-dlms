c3c2d62b8339d574c6089a2d89d6d6f119a75288
Horst Fickel <horst.fickel@qenta.com>
2023-11-10 11:43:12 +0100

    progress

--- components/dlms/dlms.cpp
+++ components/dlms/dlms.cpp
@@ -138,7 +138,7 @@ namespace esphome {
       //Get dynamic start of cipher text content, byte after the frame counter (nonce), also get dynamic length of the cipher text content
       aes.decrypt(sml_data, &dll_frame[37], 85);
 
-      uint8_t tag[12];
+      uint8_t tag[16];
       //Get 12 bytes gcm tag dynamic from the end of the frame
       memcpy(&tag[0], &dll_frame[122], sizeof(tag));
       ESP_LOGD(TAG, "GCM TAG : %s", format_hex_pretty(tag, 12).c_str());
