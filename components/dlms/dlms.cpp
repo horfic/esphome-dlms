6e2358dbd9b164bc62cda9f10856ab1ae64b6e50
Horst Fickel <horst.fickel@qenta.com>
2024-02-07 16:08:10 +0100

    work on serial number

--- components/dlms/dlms.cpp
+++ components/dlms/dlms.cpp
@@ -243,7 +243,8 @@ namespace esphome {
 
       ESP_LOGI(TAG, "test: %s", format_hex_pretty(&iv[0], 12).c_str());
       this->s_serial_number_->publish_state("test");
-      size_t serial_number = (iv[4] & 0xF) << 24 | iv[5] << 16 | iv[6] << 8 | iv[7];
+      uint32_t serial_number;
+      serial_number = ((uint32_t) iv[4] & 0xff) << 24 | (uint32_t) iv[5] << 16 | (uint32_t) iv[6] << 8 | (uint32_t) iv[7];
       //ESP_LOGI(TAG, "SML Data serial numberi: %i", serial_number);
       //ESP_LOGI(TAG, "SML Data serial numberd: %d", serial_number);
       ESP_LOGI(TAG, "SML Data serial numbers: %s", serial_number);
