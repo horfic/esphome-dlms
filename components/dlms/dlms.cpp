d204fa1a58815e8a57b0a6e64fdeb2e754133fd1
Horst Fickel <horst.fickel@qenta.com>
2024-02-07 15:44:43 +0100

    work on serial number

--- components/dlms/dlms.cpp
+++ components/dlms/dlms.cpp
@@ -241,6 +241,7 @@ namespace esphome {
         }
       }
 
+      ESP_LOGD(TAG, "test: %s", format_hex_pretty(&iv[4], 4).c_str());
       size_t serial_number = (iv[4] & 0xF) << 24 | iv[5] << 16 | iv[6] << 8 | iv[7];
       ESP_LOGI(TAG, "SML Data serial numberi: %i", serial_number);
       ESP_LOGI(TAG, "SML Data serial numberd: %d", serial_number);
