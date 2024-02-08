c75ae44db09e1419a36b6d8d6309df4094dbe9a2
Horst Fickel <horst.fickel@qenta.com>
2024-02-08 13:05:24 +0100

    refactoring and adding little uni and dlms cases

--- components/dlms/dlms.cpp
+++ components/dlms/dlms.cpp
@@ -189,8 +189,6 @@ namespace esphome {
       GCM<AESSmall128> aes;
       aes.setIV(iv, 12);
 
-      this->read_system_title(iv);
-
       uint8_t *decryption_key = this->decryption_key_.data();
       aes.setKey(decryption_key, 16);
         ESP_LOGD(TAG, "GCM Decryption Key : %s", format_hex_pretty(decryption_key, 16).c_str());
@@ -223,6 +221,8 @@ namespace esphome {
 
       ESP_LOGD(TAG, "Crypt data: %s", format_hex_pretty(&apdu[17], this->apdu_length_ - 17).c_str());
       ESP_LOGD(TAG, "Decrypted data: %s", format_hex_pretty(sml_data, sizeof(sml_data)).c_str());
+
+      //this->read_system_title(iv);
 
       // Mapping
       uint16_t Year;
@@ -376,7 +376,8 @@ namespace esphome {
       }
     }
 
-    void Dlms::read_system_title(uint8_t &iv) {
+    // ToDo - fix iv parameter, everything works inside the decrypt function but not inside here...
+    void Dlms::read_system_title(uint8_t *iv) {
       char manufacturer_id_string [3];
       char serial_number_string [16];
       uint32_t serial_number;
