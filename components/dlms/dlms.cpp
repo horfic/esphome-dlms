47baccdeb2f98a62f8cf7cb1dd3c0366f9cc4726
Horst Fickel <horst.fickel@qenta.com>
2024-02-08 16:52:37 +0100

    refactoring and adding little uni and dlms cases

--- components/dlms/dlms.cpp
+++ components/dlms/dlms.cpp
@@ -222,7 +222,7 @@ namespace esphome {
       ESP_LOGD(TAG, "Crypt data: %s", format_hex_pretty(&apdu[17], this->apdu_length_ - 17).c_str());
       ESP_LOGD(TAG, "Decrypted data: %s", format_hex_pretty(sml_data, sizeof(sml_data)).c_str());
 
-      this->read_system_title(&iv);
+      this->read_system_title(&iv[0]);
 
       // Mapping
       uint16_t Year;
